5aab7c69f9b6108467c2f30789641a6f5668bab3
arturotrejo <a.trejog@hotmail.com>
2024-12-13 15:28:57 -0500

    Add enum file

---
+++ spec/support/utils/enums.rb
@@ -0,0 +1,20 @@
+module Enums
+
+  TEXT = {
+    create_client_label: 'Create client',
+  }.freeze
+
+  LOCATION = {
+    video_office: 'Video Office'
+  }.freeze
+
+  PAGE = {
+    clients:'Clients',
+  }.freeze
+
+  SEARCH_TYPE = {
+    clients: 'Clients',
+    clients_and_contacts: 'Clients and contacts',
+  }.freeze
+
+end
